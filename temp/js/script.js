803aa6e46f141d16573eff99183892e820e80dbb
microQZR <z.h.m@outlook.com>
2021-03-11 13:03:09 -0500

    **MILESTONES: Random number array generation mechanism complete; random number array incorporated with main DOM array; major overhaul of the slider action mechanism
    CHANGES:
    *Random number array generation mechanism completed.
    *Main DOM array now dynamically inserted using JS, the child elements of which have height values proportional to elements of the associated random number array.
    *The slider handle of main DOM array's child elements now displays and dynamically updates their associated value from the random number array.
    *The "value" as stated above can be changed using the associated slider handle and the change is reflected throughout the whole system.
    *Major overhaul of the slider action mechanism, which in part significantly simplifies the "dragOn()" function. Some other parts of the slider action mechanism have also been slightly cleaned up.
    *Some light clean up of the drag-sort action mechanism.
    *Some light clean up of "temp/js/script.js"
    
    TO DO NEXT:
    *Attach event handlers to buttons
    *Start sorting algorithms
    *"settings-box" layout
    *Improve navbar

--- temp/js/script.js
+++ temp/js/script.js
@@ -1,10 +1,16 @@
+/* Inter script common variables declarations */
+let randNumArray;
+let maxNumVal;
+let maxHeight;
+
+
 /*** For main DOM array generation and sizing ***/
 
 //The following line declares independent variables for determining the main array's dimensions
 let minWidth = 10, minGap = 5, minCount = 10, maxWidth = 30;
 
-let containerMaxWidth = document.querySelector('.main-sec-container').offsetWidth;
-let arrSizeSliderVal = 0.1; //IMPORTANT - Effective main-array size controller
+let containerMaxWidth = document.querySelector('.main-sec-container').getBoundingClientRect().width;
+let arrSizeSliderVal = 0.25; //IMPORTANT - Effective main-array size controller
 let maxCount = containerMaxWidth / (minWidth + minGap);
 
 //The following lines declare the (computed) main variables of interest for determining the main array's dimensions
@@ -13,43 +19,64 @@ let width = minWidth * (maxCount / ((2 - arrSizeSliderVal) * count)) < maxWidth 
 let gap = Math.ceil(width / 2 + (5 * (1 - arrSizeSliderVal)));
 count = Math.floor(count - 2 * arrSizeSliderVal); //Adjustment code to make sure main array elements don't protrude div#main-array.main-array-box and making sure that $count is an integer value.
 
+//Defines the random number array, the max numerical value of the numbers array and the max height of the DOM "main-array"'s child elements
+randNumArray = newRandNumArray(count);
+
+maxNumVal = 100;
+maxHeight = document.querySelector('.main-array-box').getBoundingClientRect().height;
+
 //For generating the main array of poper dimensions
 let mainArrayBox = document.querySelector('.main-array-box'); //Selects the old div#main-array.main-array-box to be replaced
-
-// let arrElementHtml = `<div class="array-item" style="width: ${width}px">00</div>` //HTML content of main array elements to be inserted into div#main-array.main-array-box
-let arrElementHtml = `<div class="slider-track array-item" style="width: ${width}px">
-<div class="slider-bar drag-sort-handle" style="width: ${width}px"></div>
-<div class="slider-circ slider-handle flex" style="width: ${width + gap/2}px; height: ${width + gap/2}px; top: -${(width + gap/2) / 2}px; left: -${gap / 4}px">88</div>
-</div>` //HTML content of main array elements to be inserted into div#main-array.main-array-box
-
 let newArrayBox = mainArrayBox.cloneNode(false);
 newArrayBox.style.width = count * (width + gap) - gap + 'px'; //Set the CSS width of the new div#main-array.main-array-box
-newArrayBox.innerHTML = arrElementHtml; //This statement and the next populates the new div#main-array.main-array-box with the right number of child elements
+newArrayBox.innerHTML = `<div class="array-item" data-currentindex="0" style="width: ${width}px">
+<div class="slider-bar drag-sort-handle" style="width: ${width}px; height: ${randNumArray[0] / 100 * maxHeight}px;"></div>
+<div class="slider-circ slider-handle flex" style="width: ${width + gap/2}px; height: ${width + gap/2}px; top: ${(-(width + gap/2) / 2) + ((1 - randNumArray[0] / 100) * maxHeight)}px; left: -${gap / 4}px">${randNumArray[0]}</div>
+</div>`; //This statement and the next populates the new div#main-array.main-array-box with the right number of child elements
 for (let i = 1; i < count; i++) {
-    newArrayBox.insertAdjacentHTML('beforeend', arrElementHtml);
+    newArrayBox.insertAdjacentHTML('beforeend', `<div class="array-item" data-currentindex="${i}" style="width: ${width}px">
+    <div class="slider-bar drag-sort-handle" style="width: ${width}px; height: ${randNumArray[i] / 100 * maxHeight}px;"></div>
+    <div class="slider-circ slider-handle flex" style="width: ${width + gap/2}px; height: ${width + gap/2}px; top: ${(-(width + gap/2) / 2) + ((1 - randNumArray[i] / 100) * maxHeight)}px; left: -${gap / 4}px">${randNumArray[i]}</div>
+    </div>`); //The second argument is the HTML content of main array elements to be inserted into div#main-array.main-array-box
 }
 mainArrayBox.parentNode.replaceChild(newArrayBox, mainArrayBox); //Replaces the old div#main-array.main-array-box with the new one in the DOM tree
 
-//DEBUG LINES BELOW
-console.log("arrSizeSliderVal: ", arrSizeSliderVal);
-console.log("count: ", count);
-console.log("newArrayBox.childElementCount:", newArrayBox.childElementCount);
-// console.log("real count: ", document.getElementById('main-array').childElementCount);
-console.log("width: ", width);
-console.log("gap: ", gap);
-//DEBUG SECTION END
 
 
 /*** For generating a random number array of the proper size ***/
 function newRandNumArray(size) {
     const result = [];
-    for (let i = 0; i < size; i++) result.push(Math.random() * 10); //The constant factor multiplied to "Math.random()" determines the range of obtained values.
+    for (let i = 0; i < size; i++) result.push(Math.round(Math.random() * 100)); //The constant factor multiplied to "Math.random()" determines the range of obtained values.
     return result;
 }
 
+//Computes the mean and standard deviation of an array of numbers. Takes an array of numbers as argument. Return an array, where the 1st element is the original argument array, the 2nd element is the mean and the 3rd element is the standard deviation
+function meanAndStdDev(arr) {
+    let mean = arr.reduce((accum, elem) => accum + elem, 0) / arr.length;
+    let stdDev = Math.sqrt(arr.reduce((accum, elem) => accum + Math.pow(elem - mean, 2), 0) / arr.length);
+    return [arr, mean, stdDev];
+}
+
+
 
 //DEBUG LINES BELOW
-var testTarget = document.querySelector('.slider-target');
-// testTarget.textContent = '111';
-// testTarget.style.fo
+// console.log("arrSizeSliderVal: ", arrSizeSliderVal);
+// console.log("count: ", count);
+// console.log("newArrayBox.childElementCount:", newArrayBox.childElementCount);
+// console.log("width: ", width);
+// console.log("gap: ", gap);
+//DEBUG SECTION END
 
+
+
+//DEBUG SECTION BELOW //Testing whether Math.random() indeed generates randomly distributed numbers //!OK PASSED TEST!
+// let testCount = [0,0,0,0,0,0,0,0,0,0,0,]
+  
+// for (let i = 0; i < 1000000; i++) {
+//   testCount[Math.round(Math.random() * 10)]++;
+// }
+
+// for (let key in testCount) {
+//   console.log(`${key}: ${testCount[key]}`);
+// }
+//DEBUG SECTION END
--- temp/js/utilities.js
+++ temp/js/utilities.js
@@ -26,11 +26,9 @@ let clearTrial3 = e => {
 */
 
 //All variables declared below are defined/redefined by dragInit() and would hold values to be used by the slider drag action mechanism. (i.e. dragInit() and dragOn() )
-let offset = 0, prevPos = 0;
-let target, sliderBar;
-let targetTrack, trackInfo;
+let target, targetHalfHeight, sliderBar;
+let targetTrack, trackInfo, trackHeight;
 let highestPos, lowestPos;
-let smFinalOffset, lgFinalOffset;
 let sliderVal = 0; //Declares a globally accessible variable to hold the slider value
 
 //Drag action initialization event handler
@@ -39,21 +37,14 @@ function dragInit(e) {
 
     //Redefining all values used and manipulated by the slider drag action mechanism based on the selected slider handle
     target = this; //This is the currently selected slider handle
+    targetHalfHeight = target.getBoundingClientRect().height / 2;
     targetTrack = this.parentElement; //This is the slider track on which the handle slides
     trackInfo = targetTrack.getBoundingClientRect(); //This value holds the dimensions of the slider track
+    trackHeight = trackInfo.height;
     highestPos = trackInfo.top + window.scrollY; //$trackInfo.top is viewport coordinate; adding $window.scrollY converts it to document-page coordinate
     lowestPos = trackInfo.bottom + window.scrollY; //$trackInfo.bottom is viewport coordinate; adding $window.scrollY converts it to document-page coordinate
     sliderBar = this.previousElementSibling; //This is the "slider bar" component controlled by the current slider handle
 
-    //Assumes target's center shall be positioned at highestPos when it is at the highest position and be positioned at lowestPos when it is at the lowest position.
-    smFinalOffset = 0 - target.getBoundingClientRect().height / 2;
-    lgFinalOffset = smFinalOffset + trackInfo.height;
-
-
-    //(1) Set initial $prevPos value. (2) Eliminates positional inconsistencies between mousedown on/clicking $target on top of area within $targetTrack and mousedown on/clicking $target on top area outside of $targetTrack.
-    if (e.pageY > lowestPos) prevPos = lowestPos;
-    else if (e.pageY > highestPos) prevPos = e.pageY;
-    else prevPos = highestPos;
 
     document.onmouseup = dragFini;
     document.onmousemove = dragOn;
@@ -63,35 +54,21 @@ function dragInit(e) {
 function dragOn(e) {
     e.preventDefault();
 
-    //(1) Calculates $offset value and sets $prevPos for the next "mousemove" event. (2) Prevents out-of-sync cursor-target relationship after cursor goes beyond targetTrack. (3) Calculates $sliderVal value, which ranges from 0 to 1.
-    if (e.pageY > lowestPos) {
-        offset = lowestPos - prevPos;
-        prevPos = lowestPos;
-        sliderVal = 0;
-    } else if (e.pageY >= highestPos) {
-        offset = e.pageY - prevPos;
-        prevPos = e.pageY;
-        sliderVal = (lowestPos - e.pageY) / trackInfo.height;
-    } else {
-        offset = highestPos - prevPos;
-        prevPos = highestPos;
-        sliderVal = 1;
-    }
+    //Calculates $sliderVal value, which ranges from 0 to 1 based $e.pageY
+    if (e.pageY > lowestPos) sliderVal = 0;
+    else if (e.pageY >= highestPos) sliderVal = (lowestPos - e.pageY) / trackHeight;
+    else sliderVal = 1;
 
-    let finalOffset = target.offsetTop + offset; //Calculates the finalOffset value to be applied to CSS style
-
-    //Clips any finalOffset values that place target beyond the bounds of targetTrack
-    if (finalOffset < smFinalOffset) finalOffset = smFinalOffset;
-    if (finalOffset > lgFinalOffset) finalOffset = lgFinalOffset;
-
-    target.style.top = finalOffset + 'px'; //Applies CSS style
-    sliderBar.style.height = trackInfo.height * sliderVal + 'px';
+    target.style.top = trackHeight * (1 - sliderVal) - targetHalfHeight + 'px'; //Updates the position of the slider handle using CSS
+    target.textContent = Math.round(sliderVal * maxNumVal); //Updates the number displayed within the slider handle through the HTML content
+    sliderBar.style.height = trackHeight * sliderVal + 'px'; //Updates the position of the "slider-bar"'s height using CSS
 };
 
 //Drag action termination event handler
 function dragFini(e) {
     document.onmouseup = null;
     document.onmousemove = null;
+    randNumArray[parseInt(target.parentElement.getAttribute('data-currentindex'))] = sliderVal * maxNumVal;
 };
 
 //Attaching initialization event handlers
@@ -104,19 +81,20 @@ sliderHandles.forEach((element) => element.onmousedown = dragInit); //Attaching 
 
 let arrElemGap = gap; //**IMPORTANT** $gap is declared in "script.js"
 let arrElemWidth = width; //**IMPORTANT** $width is declared in "script.js"
-let valsToSort = [23, 1, 72, 95, 4, 36, 59, 41, 55, 32];
+let valsToSort = randNumArray //**IMPORTANT** $randNumArray is declared in "script.js"
 let elems = [...document.querySelectorAll('.drag-sort-handle')]; //A bijection exists between $elems and $valsToSort, whereby indices of $valsToSort topologically maps to the relative position of each elements of $elems within the bounds of "div.main-array-box".
 elems.forEach(elemStylePositioner); //Initially places the unsorted DOM elements within their DOM container
 
 /* CORE components below until end of section */
 let elemsContainerDimen = document.querySelector('.main-array-box').getBoundingClientRect();
 let oldIndex = 0;
-let offset2 = 0;
-let prevPos2 = 0;
 let target2 = null;
 
-//Utility function which adjusts the style of a given DOM element's !PARENT! based on a given position
-function elemStylePositioner(elem, pos) { elem.parentElement.style.left = `${pos * (arrElemWidth + arrElemGap)}px`; }; //Applying CSS style on the parent element
+//Utility function which adjusts the style of a given DOM element's !PARENT! based on a given position and updates that parent element's "data-currentindex" attribute
+function elemStylePositioner(elem, pos) {
+    elem.parentElement.style.left = `${pos * (arrElemWidth + arrElemGap)}px`;
+    elem.parentElement.setAttribute('data-currentindex', pos + "");
+};
 
 //Utility function which updates the position of elements within $elems and $valsToSort arrays upon a drag-sort action
 function arraysUpdateOnDrag(oldIndex, newIndex) {
@@ -128,7 +106,6 @@ function arraysUpdateOnDrag(oldIndex, newIndex) {
 let dragSortInit = function(e) {
     e.preventDefault();
 
-    prevPos2 = e.pageX;
     oldIndex = Math.floor((e.pageX - elemsContainerDimen.left) / (arrElemWidth + arrElemGap));
     target2 = this; //Registers the active DOM element being dragged 
 
@@ -148,14 +125,6 @@ function dragSortOn(e) {
     
     if (e.clientX <= elemsContainerDimen.left || e.clientX >= elemsContainerDimen.right) return; //This bounds the drag-sort motion of the active DOM element to within its DOM container.
 
-    //***DEPRECATED. The following segment is not needed
-    // //Below is for the <<motion>> of the selected DOM element
-    // offset2 = e.pageX - prevPos2; //Calculates the delta offset2
-    // prevPos2 = e.pageX; //Sets the $prevPos2 value for use for the next event
-
-    // let finalOffset = target2.offsetLeft + offset2;
-    // target2.style.left = finalOffset + 'px'; //Applies CSS style
-    //***
 
     //Below is for the proper positioning of all affected DOM elements upon drag over
     let newIndex = Math.floor((e.pageX - elemsContainerDimen.left) / (arrElemWidth + arrElemGap));

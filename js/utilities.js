b27c5fc80fefcee621fac328e4972c9436daea30
microQZR <z.h.m@outlook.com>
2021-04-08 17:37:42 -0400

    **IMPORTANT: Contents of "temp/" now replace contents of root

--- js/utilities.js
+++ js/utilities.js
@@ -1,57 +1,59 @@
-/*** For swap animation/transition ***/
-let trial1 = (e) => {
-    document.getElementById('test6').classList.add('animate');
-    console.log('OK');
+/*** General Utility Functions ***/
+function disableSlideDrag() {
+    document.querySelectorAll('.slider-handle').forEach((element) => element.onmousedown = null); //Detaching the drag action initializer to all DOM elements with class="slider-handle"
+    elems.forEach(elem => { elem.onmousedown = null; }); //Detaching the drag-sort action initialization event handler from each array element of $elems
 };
-let trial2 = (e) => {
-    document.getElementById('test6').style.animation = "trial2 1s forwards";
-    console.log('OK');
+
+function enableSlideDrag() {
+    document.querySelectorAll('.slider-handle').forEach((element) => element.onmousedown = dragInit); //Attaching the drag action initializer to all DOM elements with class="slider-handle"
+    elems.forEach(elem => { elem.onmousedown = dragSortInit; }); //Attaching the drag-sort action initialization event handler to each array element of $elems
 };
-let clearTrial2 = e => {
-    document.getElementById('test6').style.animation = 'none';
-};
-let trial3 = e => {
-    document.getElementById('test6').style.right = '50px';
-    document.getElementById('test6').style.backgroundColor = 'red';
-}
-let clearTrial3 = e => {
-    document.getElementById('test6').style.right = '0px';
-    document.getElementById('test6').style.backgroundColor = 'var(--main-purple)';
+
+function resetArrayItemsColor() {
+    elems.forEach(elem => elem.style.backgroundColor = "var(--main-blue)");
+    if (activeSortHandle.id == 'insertion-sort') elems.forEach(elem => elem.style.boxShadow = "none");
 }
 
+let initialTimerID;
+function cancelPendingJobs() {
+    let currentTimerID = setTimeout(() => {});
+    for (let i = initialTimerID; i <= currentTimerID; i++) clearTimeout(i);
+
+    //For smoothly clearing up any potentially active info dialog, since the usual info dialog presentation process is also affected by the statements above
+    activeInfoDialog.style.opacity = "0%";
+    setTimeout(() => activeInfoDialog.style.top = "-100px", 500);
+};
+
+function fixTextOverflow(sliderHandle) {
+    const numberDisplay = sliderHandle.firstChild;
+    if (Math.hypot(numberDisplay.offsetWidth, numberDisplay.offsetHeight) > sliderHandle.offsetWidth) numberDisplay.textContent = "#";
+    if (Math.hypot(numberDisplay.offsetWidth, numberDisplay.offsetHeight) > sliderHandle.offsetWidth) numberDisplay.textContent = "";
+}
 
 
-/*** For slider drag action ***
-*!Functional, but noticeable CPU usage under continuous drag @ ~5% CPU load. May be enhanced.
-*/
-
-//Extra/non-core components
-let elemBar = document.getElementById('elem-bar');
-let elemBarDimen = elemBar.getBoundingClientRect();
-
-//CORE components
-let offset = 0, prevPos = 0;
-let target = document.getElementById('target'); //ATM the slider thumb is an HTML element with id="target"
-let targetTrack = document.getElementById('track'); //ATM the slider track is an HTML element with id="track"
-let trackInfo = targetTrack.getBoundingClientRect();
-let highestPos = trackInfo.top + window.scrollY; //$trackInfo.top is viewport coordinate; adding $window.scrollY converts it to document-page coordinate
-let lowestPos = trackInfo.bottom + window.scrollY; //$trackInfo.bottom is viewport coordinate; adding $window.scrollY converts it to document-page coordinate
-
-//Assumes target's center shall be positioned at highestPos when it is at the highest position and be positioned at lowestPos when it is at the lowest position.
-let smFinalOffset = 0 - target.getBoundingClientRect().height / 2;
-let lgFinalOffset = smFinalOffset + trackInfo.height;
-
-//Define a globally accessible variable to hold the slider value
-let sliderVal = 0;
+/*** For Slider Drag Action ***/
+//All variables declared below are defined/redefined by dragInit() and would hold values to be used by the slider drag action mechanism. (i.e. dragInit() and dragOn() )
+let target, targetHalfHeight, sliderBar;
+let targetTrack, trackInfo, trackHeight;
+let highestPos, lowestPos;
+let sliderVal = 0; //Declares a globally accessible variable to hold the slider value
+let hasNotMoved = true;
 
 //Drag action initialization event handler
 function dragInit(e) {
     e.preventDefault();
+    hasNotMoved = true;
 
-    //(1) Set initial $prevPos value. (2) Eliminates positional inconsistencies between mousedown on/clicking $target on top of area within $targetTrack and mousedown on/clicking $target on top area outside of $targetTrack.
-    if (e.pageY > lowestPos) prevPos = lowestPos;
-    else if (e.pageY > highestPos) prevPos = e.pageY;
-    else prevPos = highestPos;
+    //Redefining all values used and manipulated by the slider drag action mechanism based on the selected slider handle
+    target = this; //This is the currently selected slider handle
+    targetHalfHeight = target.getBoundingClientRect().height / 2;
+    targetTrack = this.parentElement; //This is the slider track on which the handle slides
+    trackInfo = targetTrack.getBoundingClientRect(); //This value holds the dimensions of the slider track
+    trackHeight = trackInfo.height;
+    highestPos = trackInfo.top + window.scrollY; //$trackInfo.top is viewport coordinate; adding $window.scrollY converts it to document-page coordinate
+    lowestPos = trackInfo.bottom + window.scrollY; //$trackInfo.bottom is viewport coordinate; adding $window.scrollY converts it to document-page coordinate
+    sliderBar = this.previousElementSibling; //This is the "slider bar" component controlled by the current slider handle
+
 
     document.onmouseup = dragFini;
     document.onmousemove = dragOn;
@@ -60,62 +62,113 @@ function dragInit(e) {
 //Drag action core event handler
 function dragOn(e) {
     e.preventDefault();
+    hasNotMoved = false;
 
-    //(1) Calculates $offset value and sets $prevPos for the next "mousemove" event. (2) Prevents out-of-sync cursor-target relationship after cursor goes beyond targetTrack. (3) Calculates $sliderVal value, which ranges from 0 to 1.
-    if (e.pageY > lowestPos) {
-        offset = lowestPos - prevPos;
-        prevPos = lowestPos;
-        sliderVal = 0;
-    } else if (e.pageY >= highestPos) {
-        offset = e.pageY - prevPos;
-        prevPos = e.pageY;
-        sliderVal = (lowestPos - e.pageY) / trackInfo.height;
-    } else {
-        offset = highestPos - prevPos;
-        prevPos = highestPos;
-        sliderVal = 1;
-    }
+    //Calculates $sliderVal value, which ranges from 0 to 1 based $e.pageY
+    if (e.pageY > lowestPos) sliderVal = 0;
+    else if (e.pageY >= highestPos) sliderVal = (lowestPos - e.pageY) / trackHeight;
+    else sliderVal = 1;
 
-    // console.log(sliderVal); //DEBUG LINE//OK PASSED TEST
-
-    let finalOffset = target.offsetTop + offset; //Calculates the finalOffset value to be applied to CSS style
-
-    //Clips any finalOffset values that place target beyond the bounds of targetTrack
-    if (finalOffset < smFinalOffset) finalOffset = smFinalOffset;
-    if (finalOffset > lgFinalOffset) finalOffset = lgFinalOffset;
-
-    target.style.top = finalOffset + 'px'; //Applies CSS style
-    elemBar.style.height = elemBarDimen.height * sliderVal + 'px';
+    target.style.top = trackHeight * (1 - sliderVal) - targetHalfHeight + 'px'; //Updates the position of the slider handle using CSS
+    target.firstChild.textContent = Math.round(sliderVal * maxNumVal); //Updates the number displayed within the slider handle through the HTML content
+    fixTextOverflow(target); //Prevents the overflowing of textContent of the span.number-display within $target
+    sliderBar.style.height = trackHeight * sliderVal + 'px'; //Updates the position of the "slider-bar"'s height using CSS
 };
 
 //Drag action termination event handler
 function dragFini(e) {
     document.onmouseup = null;
     document.onmousemove = null;
+    if (hasNotMoved) return;
+
+    randNumArray[parseInt(target.parentElement.getAttribute('data-currentindex'))] = Math.round(sliderVal * maxNumVal);
+    
+    let roundedSliderVal = Math.round(sliderVal * maxNumVal) / maxNumVal;
+    target.style.top = trackHeight * (1 - roundedSliderVal) - targetHalfHeight + 'px'; //Updates the position of the slider handle using CSS
+    sliderBar.style.height = trackHeight * roundedSliderVal + 'px'; //Updates the position of the "slider-bar"'s height using CSS
+
 };
 
-target.onmousedown = dragInit; //Attaching the drag action initializer to $target
+//Attaching initialization event handlers
+document.querySelectorAll('.slider-handle').forEach((element) => {
+    fixTextOverflow(element); //Prevents the overflowing of textContent of the span.number-display within $target
+    element.onmousedown = dragInit; //Attaching the drag action initializer to all DOM elements with class="slider-handle"
+});
 
+
+/*** For Horizontal Sliders of Settings Box ***/
+let trackWidth, leftmostPos, rightmostPos
+
+//Horizontal slider drag action initialization event handler
+function h_dragInit(e) {
+    e.preventDefault();
+
+    //Redefining all values used and manipulated by the slider drag action mechanism based on the selected slider handle
+    target = this; //This is the currently selected slider handle
+    targetHalfHeight = target.getBoundingClientRect().height / 2;
+    targetTrack = this.parentElement; //This is the slider track on which the handle slides
+    trackInfo = targetTrack.getBoundingClientRect(); //This value holds the dimensions of the slider track
+    trackWidth = trackInfo.width;
+    leftmostPos = trackInfo.left + window.scrollX; //$trackInfo.left is viewport coordinate; adding $window.scrollX converts it to document-page coordinate
+    rightmostPos = trackInfo.right + window.scrollX; //$trackInfo.right is viewport coordinate; adding $window.scrollX converts it to document-page coordinate
+
+
+    document.onmouseup = h_dragFini;
+    document.onmousemove = h_dragOn;
+};
+
+//Drag action core event handler
+function h_dragOn(e) {
+    e.preventDefault();
+
+    //Calculates $sliderVal value, which ranges from 0 to 1 based on $e.pageX
+    if (e.pageX < leftmostPos) sliderVal = 0;
+    else if (e.pageX <= rightmostPos) sliderVal = (e.pageX - leftmostPos) / trackWidth;
+    else sliderVal = 1;
+
+    target.style.left = trackWidth * sliderVal - targetHalfHeight + 'px'; //Updates the position of the slider handle using CSS
+
+    if (e.pageX >= leftmostPos && e.pageX <= rightmostPos && target.id === "size-handle") {
+        arrSizeSliderVal = sliderVal;
+        calculateWidthGapCount();
+        updateMainArrayContent(1);
+    }
+};
+
+//Drag action termination event handler
+function h_dragFini(e) {
+    document.onmouseup = null;
+    document.onmousemove = null;
+
+    if (target.id === "size-handle") {
+        console.log("size:", sliderVal)
+    } else {
+        speedSliderVal = sliderVal;
+        showInfoDialog('success-msg-1');
+        console.log("speed:", sliderVal)
+    }
+};
+
+//Attaching initialization event handlers
+document.querySelectorAll('.h-slider-handle').forEach((element) => element.onmousedown = h_dragInit); //Attaching the drag action initializer to all DOM elements with class="h-slider-handle"
 
 
 /*** For drag-sort drag action ***/
+//Declaration and definition of tweakable, independent variables
+let arrElemGap = gap; //**IMPORTANT** $gap is declared in "script.js"
+let arrElemWidth = width; //**IMPORTANT** $width is declared in "script.js"
+let valsToSort = randNumArray //**IMPORTANT** $randNumArray is declared in "script.js"
 
-// let arrElemCount = 10; //The variable is currently not. Preserved for possible future usage.
-let arrElemGap = 5;
-let arrElemWidth = 20;
-let valsToSort = [23, 1, 72, 95, 4, 36, 59, 41, 55, 32];
-let elems = [...document.querySelectorAll('.testing')]; //A bijection exists between $elems and $valsToSort, whereby indices of $valsToSort topologically maps to the relative position of each elements of $elems within the bounds of "div.testbox-inner".
-elems.forEach(elemStylePositioner); //Initially places the unsorted DOM elements within their DOM container
+//Declaration and definition of non-tweakable variables
+let elems = [...document.querySelectorAll('.drag-sort-handle')]; //A bijection exists between $elems and $valsToSort, whereby indices of $valsToSort topologically maps to the relative position of each elements of $elems within the bounds of "div.main-array".
+let elemsContainerDimen = document.querySelector('.main-array').getBoundingClientRect();
+let oldIndex, target2;
 
-/* CORE components below until end of section */
-let elemsContainerDimen = document.querySelector('.testbox-inner').getBoundingClientRect();
-let oldIndex = 0;
-let offset2 = 0;
-let prevPos2 = 0;
-let target2 = null;
-
-//Utility function which adjusts the style of a given DOM element based on a given position
-function elemStylePositioner(elem, pos) { elem.style.left = `${pos * (arrElemWidth + arrElemGap)}px`; };
+//Utility function which adjusts the style of a given DOM element's !PARENT! based on a given position and updates that parent element's "data-currentindex" attribute
+function elemStylePositioner(elem, pos) {
+    elem.parentElement.style.left = `${pos * (arrElemWidth + arrElemGap)}px`;
+    elem.parentElement.setAttribute('data-currentindex', pos + "");
+};
 
 //Utility function which updates the position of elements within $elems and $valsToSort arrays upon a drag-sort action
 function arraysUpdateOnDrag(oldIndex, newIndex) {
@@ -124,17 +177,17 @@ function arraysUpdateOnDrag(oldIndex, newIndex) {
 };
 
 //Drag-sort action initialization event handler
-let dragSortInit = function(e) {
+function dragSortInit(e) {
     e.preventDefault();
 
-    prevPos2 = e.pageX;
     oldIndex = Math.floor((e.pageX - elemsContainerDimen.left) / (arrElemWidth + arrElemGap));
     target2 = this; //Registers the active DOM element being dragged 
 
-    //Sets special CSS style for the active DOM element being dragged
-    target2.style.zIndex = 100;
+    //Sets special CSS style for the active DOM element being dragged !AND ITS PARENT!
+    target2.parentElement.style.zIndex = 100; //Applying CSS property on the parent instead
+    target2.parentElement.style.transition = 'none'; //Applying CSS property on the parent instead
     target2.style.boxShadow = '0px 0px 5px 5px var(--main-green)';
-    target2.style.transition = 'none';
+    document.getElementById('main-array').style.cursor = 'pointer';
 
     //Attaches subcomponent event handlers
     document.onmouseup = dragSortFini;
@@ -146,15 +199,6 @@ function dragSortOn(e) {
     e.preventDefault();
     
     if (e.clientX <= elemsContainerDimen.left || e.clientX >= elemsContainerDimen.right) return; //This bounds the drag-sort motion of the active DOM element to within its DOM container.
-
-    //***DEPRECATED. The following segment is not needed
-    // //Below is for the <<motion>> of the selected DOM element
-    // offset2 = e.pageX - prevPos2; //Calculates the delta offset2
-    // prevPos2 = e.pageX; //Sets the $prevPos2 value for use for the next event
-
-    // let finalOffset = target2.offsetLeft + offset2;
-    // target2.style.left = finalOffset + 'px'; //Applies CSS style
-    //***
 
     //Below is for the proper positioning of all affected DOM elements upon drag over
     let newIndex = Math.floor((e.pageX - elemsContainerDimen.left) / (arrElemWidth + arrElemGap));
@@ -175,15 +219,44 @@ function dragSortOn(e) {
     }
 };
 
-//Drag action core event handler
-let dragSortFini = function(e) {
+//Drag-sort action termination event handler
+function dragSortFini(e) {
     //Detach subcomponent event handlers
     document.onmouseup = null;
     document.onmousemove = null;
     //Restore initial CSS styles
-    target2.style.zIndex = "auto";
+    target2.parentElement.style.zIndex = "auto"; //Applying CSS property on the !PARENT! instead
+    target2.parentElement.style.transition = "0.5s ease-out"; //Applying CSS property on the !PARENT! instead
     target2.style.boxShadow = "none";
-    target2.style.transition = "0.5s ease-out";
+    document.getElementById('main-array').style.cursor = 'initial';
 };
 
+//Actually executing previously defined code
+elems.forEach(elemStylePositioner); //Initially places the unsorted DOM elements within their DOM container
 elems.forEach(elem => { elem.onmousedown = dragSortInit; }); //Attaching the drag-sort action initialization event handler to each array element of $elems
+
+
+/*** For Informational Dialogs Presentation ***/
+let dialogTimerID;
+let activeInfoDialog = document.getElementById('success-msg-1');
+function showInfoDialog(infoDialogID) {
+    dismissInfoDialog();
+    activeInfoDialog = document.getElementById(infoDialogID);
+    activeInfoDialog.style.transition = "500ms ease-out";
+    activeInfoDialog.style.opacity = "100%";
+    activeInfoDialog.style.top = "30px";
+
+    dialogTimerID = setTimeout(() => {
+        activeInfoDialog.style.opacity = "0%";
+        dialogTimerID = setTimeout(() => activeInfoDialog.style.top = "-100px", 500);
+    }, 3000);
+}
+
+//Event handler for the dismiss button on each info dialog
+function dismissInfoDialog() {
+    clearTimeout(dialogTimerID);
+    activeInfoDialog.style.transition = "none";
+    activeInfoDialog.style.opacity = "0%";
+    activeInfoDialog.style.top = "-100px";
+}
+document.querySelectorAll('.info-dial-dismiss').forEach(value => value.addEventListener("click", dismissInfoDialog));
